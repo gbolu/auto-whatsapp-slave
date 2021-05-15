7fc67c88fc14b384847018c0d03a682ae3c37ac5
Gbolu Adeyemi <gboluwagaadeyemi@gmail.com>
2021-05-15 18:51:45 +0100

    Implement method to print active chrome instances

--- closeChromeInstance.py
+++ closeChromeInstance.py
@@ -6,9 +6,10 @@ def closeChrome(target_process):
     output, error = temp_subprocess.communicate()
 
     for line in output.splitlines():
-        if target_process in str(line):
-            pid = int(line.split(None, 1)[0])
-            print(pid)
+        # if target_process in str(line):
+        #     pid = int(line.split(None, 1)[0])
+        #     print(pid)
+        print(line)
 
 if __name__ == "__main__":
     target_process = sys.argv[1]
