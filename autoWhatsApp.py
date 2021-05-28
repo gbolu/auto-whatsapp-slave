dfd9d154b891bbafe949c5e4b139a1d3d45b96d3
Gbolu Adeyemi <gboluwagaadeyemi@gmail.com>
2021-05-28 10:29:23 +0100

    Update user-data-dit

--- autoWhatsApp.py
+++ autoWhatsApp.py
@@ -9,7 +9,7 @@ def autoWhatsApp(user_profile_path, phone_number='2348100415220', message='', ex
     options = webdriver.ChromeOptions()
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-using')
-    options.add_argument(user_profile_path)
+    # options.add_argument(user_profile_path)
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-extensions")
     options.add_argument("--remote-debugging-port=9222")
