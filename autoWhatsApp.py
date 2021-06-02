f8663522cbed96fd9c63c89d4ddba968da067396
Gbolu Adeyemi <gboluwagaadeyemi@gmail.com>
2021-06-02 18:56:20 +0100

    Add missing message error to autoWhatsapp

--- autoWhatsApp.py
+++ autoWhatsApp.py
@@ -31,6 +31,8 @@ def autoWhatsApp(user_profile_path, phone_number='2348100415220', message='', ex
     WebDriverWait(driver, 60, 0.5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[2]'))).send_keys(message)
     WebDriverWait(driver, 60, 0.5).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[3]/button'))).click()
     # close any lingering processes 
+    if message not in driver.page_source:
+        raise Exception("Failed to send message.")
     try:
         driver.close()
         driver.quit()   
