22f8d3ecd12451ba4d7e4ae66398155743ecab97
Gbolu Adeyemi <gboluwagaadeyemi@gmail.com>
2021-06-08 16:41:10 +0100

    Revert change to autoWhatsApp driver close

--- autoWhatsApp.py
+++ autoWhatsApp.py
@@ -26,7 +26,6 @@ def autoWhatsApp(user_profile_path, phone_number='2348100415220', message='', ex
     executable_path=executable_path, options=options)
 
     driver.get("https://web.whatsapp.com/send?phone={}".format(phone_number))
-    time.sleep(30)
 
     WebDriverWait(driver, 60, 0.5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[2]'))).send_keys(message)
     WebDriverWait(driver, 60, 0.5).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[3]/button'))).click()
@@ -37,9 +36,9 @@ def autoWhatsApp(user_profile_path, phone_number='2348100415220', message='', ex
         driver.close()
         driver.quit()   
     finally:
-        print('Done')
+        print('Message to {} has been sent!'.format(phone_number))
         pass
 if __name__ == '__main__':
-    autoWhatsApp("--user-data-dir=/home/gboluwagaadeyemi/code_files/auto_whatsapp/user-data", 
+    autoWhatsApp("--user-data-dir=/home/gboluwagaadeyemi/code_files/auto_whatsapp_slave/user-data", 
         phone_number=sys.argv[1], message=sys.argv[2])
     closeProcess(findProcessPid("chrome"))
