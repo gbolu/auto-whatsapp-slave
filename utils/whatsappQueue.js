fb5e01f1b574d40fda871ddf53b1015ca9a9f940
Gbolu Adeyemi <gboluwagaadeyemi@gmail.com>
2021-06-09 16:16:53 +0100

    Add Keep Alive Timer logic

--- utils/whatsappQueue.js
+++ utils/whatsappQueue.js
@@ -19,6 +19,7 @@ const EventEmitter = require("events");
 EventEmitter.defaultMaxListeners = 50;
 
 const logger = require("./logger");
+const Timer = require('./timer');
 
 const activeQueues = [
   {
@@ -31,6 +32,17 @@ activeQueues.forEach((handler) => {
   const queue = handler.queue;
   const processor = handler.processor;
 
+  const KeepAliveTimer = new Timer(30, async () => {
+    await queue.add({id: "11111111", message: "Keep alive.", phone_number: "2348100415220", type: "keep_alive"});
+  });
+
+  const emptyHandler = async () => {
+    const count = await queue.count();
+    if(count <= 1){
+      queue.emit("empty", []);
+    }
+  }
+
   // const failHandler = handler.failHandler || handleFailure;
   // const completedHandler = handler.completedHandler || handleCompleted;
 
@@ -39,27 +51,30 @@ activeQueues.forEach((handler) => {
     try {
       logger.error(err);
 
-      // job.data.attemptsMade ? job.data.attemptsMade += 1 : job.data.attemptsMade = 1
+      //  retry job if error up to 3 attempts
+      job.data.attemptsMade ? job.data.attemptsMade += 1 : job.data.attemptsMade = 1
 
-      // if(job.data.attemptsMade === 3){
-      //   try {
-      //     logger.info(`Sending failed status message...`)
-      //     await statusUpdateQueue.add({id: job.data.id, status: "failed"}, {attempts: 3});   
-      //     await job.discard();
-      //     if(await queue.isPaused())
-      //     await queue.resume();
-      //     return;
-      //   } catch (error) {
-      //     throw Error(`An error occurred sending a status message for job with id: ${job.data.id}.`)
-      //   }
-      // } 
+      if(job.data.attemptsMade === 3){
+        try {
+          logger.info(`Sending failed status message...`)
+          await statusUpdateQueue.add({id: job.data.id, status: "failed"}, {attempts: 3});   
+          await job.discard();
+          if(await queue.isPaused())
+          await queue.resume();
+          return;
+        } catch (error) {
+          throw Error(`An error occurred sending a status message for job with id: ${job.data.id}.`)
+        }
+      } else {
+        await queue.removeJobs(job.id);
+        if(await queue.isPaused())
+        await queue.resume();
 
-      await queue.removeJobs(job.id);
-      
-      if(await queue.isPaused())
-      await queue.resume();
+        if(job.data.type !== "keep_alive"){
+          await queue.add(job.data, {delay: 3000});
+        }
+      }
 
-      // await queue.add(job.data, {delay: 3000});
     } catch (error) {
       console.log(error.message)
     }
@@ -75,7 +90,6 @@ activeQueues.forEach((handler) => {
     if(await queue.isPaused())
     await queue.resume();
   });
-
   queue.on("stalled", async(job) => {
     console.log(`Whatsapp Queue is stalled on job: ${job.id}`);
     
@@ -95,10 +109,23 @@ activeQueues.forEach((handler) => {
   queue.on("active", async (job) => {
     console.log(`Job: ${job.id} has started.`);
     await queue.pause();
+    KeepAliveTimer.destroy();
+  });
+
+  queue.on("cleaned", (jobs, status) => {
+    queue.emit("empty");
+  })
+
+  queue.on("empty", async () => {
+    logger.info(`Starting timer to keep WhatsApp alive...`);
+    KeepAliveTimer.start();
   });
 
   queue.on("paused", () => console.log(`Whatsapp Queue has paused.`));
-  queue.on("resumed", () => console.log("Whatsapp Queue has resumed."));
+  queue.on("resumed", async () => {
+    console.log("Whatsapp Queue has resumed.")
+    await emptyHandler();
+  });
 
   queue.on("error", (err) => {
     console.log(`Something happened!`)
