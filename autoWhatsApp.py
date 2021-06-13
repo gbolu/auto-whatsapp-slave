0d09ffa67e465421cf8e88a4e28894fd5c5d63ea
Gbolu Adeyemi <gboluwagaadeyemi@gmail.com>
2021-06-13 16:32:19 +0100

    Add new line replacement in message to whatsapp.

--- autoWhatsApp.py
+++ autoWhatsApp.py
@@ -32,8 +32,8 @@ def autoWhatsApp(user_profile_path, phone_number='2348100415220', message='', ex
     messages = message.split('\n')
 
     driver.get("https://web.whatsapp.com/send?phone={}".format(phone_number))
+    textElement = WebDriverWait(driver, 60, 0.5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[2]')))
     for text in messages:
-        textElement = WebDriverWait(driver, 60, 0.5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[2]')))
         ActionChains(driver).click(textElement)
         textElement.send_keys(text)
         ActionChains(driver).send_keys(Keys.SHIFT, Keys.ENTER)
