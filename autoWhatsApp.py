0342a4aebfc94b21f79a7955fc9b7c149893e6c9
Gbolu Adeyemi <gboluwagaadeyemi@gmail.com>
2021-06-22 12:38:30 +0100

    Fix error on autoWhatsapp

--- autoWhatsApp.py
+++ autoWhatsApp.py
@@ -54,9 +54,10 @@ def autoWhatsApp(user_profile_path, phone_number='2348100415220', message='', ex
     WebDriverWait(driver, 60, 0.5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[3]/button'))).click()
     # close any lingering processes 
     for text in messages:
-        if text not in driver.page_source:
-            raise Exception("Failed to send message.")
+        if text in driver.page_source:
             break
+        else:
+            raise Exception("Failed to send message!")        
     try:
         driver.close()
         driver.quit()   
