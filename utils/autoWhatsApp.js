a57d5f6358b4fc1e6a1c8662351c3279e9045a55
Gbolu Adeyemi <gboluwagaadeyemi@gmail.com>
2021-07-14 17:04:19 +0100

    Investigate source of chromedriver error on server

--- utils/autoWhatsApp.js
+++ utils/autoWhatsApp.js
@@ -35,47 +35,47 @@ class AutoWhatsapp {
         //  navigate to the whatsapp web webpage in a new tab
         try {
             await this.driver.executeScript(`window.open('https://web.whatsapp.com/send?phone=${phone_number}')`);
-            
+            let windowHandles = await this.driver.getAllWindowHandles();
+            let baseWindowHandle = await this.driver.getWindowHandle();
+            let targetWindowHandle = windowHandles.filter(windowHandle => windowHandle != baseWindowHandle)[0];
+            await this.driver.switchTo().window(targetWindowHandle);
+
+            //  select text field used to input messages
+            let textElement = await this.driver.wait(until.elementLocated(By.xpath('/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[1]/div/div[2]')))  
+
+            for(let text of messages)
+            {
+                //  add text to text field
+                await textElement.sendKeys(text);
+
+                //  click send button
+                let clickButtonElement = await this.driver.wait(until.elementLocated(By.xpath('/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[2]/button')));
+                await clickButtonElement.click();
+            }
+
+            messages = message.replace('\n', '')
+            let source = await this.driver.getPageSource();
+
+            for(let messageText of messages)
+            {
+                if (source.includes(messageText))
+                break
+
+                else
+                throw Error("Failed to send message!");
+            }
+
+            try {
+                await this.driver.close();
+                await this.driver.switchTo().window(baseWindowHandle);
+            } catch (error) {
+                console.log(error);
+            }
         } catch (error) {
-            
+            console.log(err);
+            await Promise.reject(error);
         }
-        let windowHandles = await this.driver.getAllWindowHandles();
-        let baseWindowHandle = await this.driver.getWindowHandle();
-        let targetWindowHandle = windowHandles.filter(windowHandle => windowHandle != baseWindowHandle)[0];
-        await this.driver.switchTo().window(targetWindowHandle);
-
-        //  select text field used to input messages
-        let textElement = await this.driver.wait(until.elementLocated(By.xpath('/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[1]/div/div[2]')))  
-
-        for(let text of messages)
-        {
-            //  add text to text field
-            await textElement.sendKeys(text);
-
-            //  click send button
-            let clickButtonElement = await this.driver.wait(until.elementLocated(By.xpath('/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[2]/button')));
-            await clickButtonElement.click();
-        }
-
-        messages = message.replace('\n', '')
-        let source = await this.driver.getPageSource();
-
-        for(let messageText of messages)
-        {
-            if (source.includes(messageText))
-            break
-
-            else
-            throw Error("Failed to send message!");
-        }
-
-        try {
-            await this.driver.close();
-            await this.driver.switchTo().window(baseWindowHandle);
-        } catch (error) {
-            console.log(error);
-        }
-
+        
         await Promise.resolve();
     }
 }
